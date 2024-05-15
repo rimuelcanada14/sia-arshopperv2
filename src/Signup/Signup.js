27614f68d35980ecd4b9cc092ad26659044c6c1f
rimuelcanada14 <102201493+rimuelcanada14@users.noreply.github.com>
2024-05-15 15:36:10 +0800

    SIGNUP FRONTEND ANGAS

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -6,75 +6,7 @@ import {Link} from 'react-router-dom';
 import './Signup.css';
 import Header from '../components/header';
 
-// const InputSignupFName = (props) => {
-//   return (
-//       <>
-//           <div className = "signup-user">
-//               <FaUser className="signup-icon-user" />
-//               <input autoComplete = "on" type = "text" placeholder ="First Name" id='signup-input-user'></input>
-//           </div>
-          
-//       </>
-//   )
-// }
-// const InputSignupLName = (props) => {
-//   return (
-//       <>
-//           <div className = "signup-user">
-//               <FaUser className="signup-icon-user" />
-//               <input autoComplete = "on" type = "text" placeholder ="Last name" id='signup-input-user'></input>
-//           </div>
-          
-//       </>
-//   )
-// }
-// const InputSignupMobile = (props) => {
-//     return (
-//         <>
-//             <div className = "signup-mobile">
-//                 <FaMobileRetro className="signup-icon-mobile" />
-//                 <p className = "signup-p-mobile">+63</p>
-//                 <input autoComplete = "on" type = "number" placeholder ="9151234567" id='signup-input-mobile' maxlength="10"></input>
-//             </div>
-            
-//         </>
-//     )
-// }
-// const InputSignupCrPass = (props) => {
-//     return (
-//         <>  
-//             <div className = "signup-pass">
-//                 <FaLock className="signup-icon-pass" />
-//                 <input type = "password" placeholder ="Create Password" id='signup-input-pass'></input>
-//             </div>
-            
-//         </>
-//     )
-// }
-// const InputSignupCoPass = (props) => {
-//   return (
-//       <>  
-//           <div className = "signup-pass">
-//               <FaLockOpen className="signup-icon-pass" />
-//               <input type = "password" placeholder ="Confirm Password" id='signup-input-pass'></input>
-//           </div>
-          
-//       </>
-//   )
-// }
-// const InputSignupHealth = (props) => {
-//   return (
-//     <>
-//       <div className="signup-health">
-//         <FaNotesMedical className="signup-icon-health" />
-//         <select id="signup-input-health">
-//           <option value="strong">I have health complication/s</option>
-//           <option value="medium">I do not have health complications</option>
-//         </select>
-//       </div>
-//     </>
-//   );
-// };
+//FOR BACK-END
 const Signup = () => {
   const [formData, setFormData] = useState({
     firstName: '',
@@ -86,8 +18,6 @@ const Signup = () => {
   });
 
   const [passErr, setPassError] = useState('');
-
-
 
   const handleChange = (e) => {
     const{name, value} = e.target;
@@ -119,101 +49,111 @@ const Signup = () => {
       console.error('Failed to sign up', error);
     }
   };
-// function Login() {
-//   const [setMessage] = useState('');
 
-//   useEffect(() => {
-//     axios.get('http://localhost:8000/api/hello-world/')
-//       .then(response => {
-//         setMessage(response.data.message);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }, [setMessage]);
-// }
+//FOR FRONT-END
   return (
-    // <>
-    //     <div className='signup-header'>
-    //         <Header header="ARShopper"/>
-    //     </div>
-        
-    //     <div className='signup-page'>
-    //         <h1 className = "signup-title">SIGN UP</h1>
-    //         <div className = "signup-container">
-    //             <p className = "signup-categories">First Name</p>
-    //             <InputSignupFName />
-    //             <p className = "signup-categories">Last Name</p>
-    //             <InputSignupLName />
-    //             <p className = "signup-categories">Mobile Number</p>
-    //             <InputSignupMobile />
-    //             <p className = "signup-categories">Create Password</p>
-    //             <InputSignupCrPass />
-    //             <p className = "signup-categories">Confirm Password</p>
-    //             <InputSignupCoPass />
-    //             <p className = "signup-categories">Password</p>
-    //             <InputSignupHealth />
-    //         </div>
-
-    //         <div className='signup-low'>
-    //             <Link to ="/home" className = "signup-submit">SUBMIT</Link>
-    //             <p>Already have an account?<Link to ="/" className = "signup-login">Login</Link></p>
-    //         </div>
-    //     </div>
-      
-    // </>
     <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-        
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-        
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          onChange={handleChange}
-        />
+      <div className='signup-header'>
+        <Header header="ARShopper" headerright="ICHI MART" headersub="©"/>
+      </div>
 
-        {passErr && <div style={{color: 'red'}}>{passErr} </div>}
-        
-        <input
-          type="number"
-          name="mobileNumber"
-          placeholder="Mobile Number"
-          value={formData.mobileNumber}
-          onChange={handleChange}
-        />
-        
-        <select name="healthComplication" id="dropdown" onChange={handleChange} value={formData.healthComplication}>
-          <option value="yes">I Have Health Complications</option>
-          <option value ="no">I Don't Have Health Complications</option>
-        </select>
-        
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+      <div className='signup-page'>
+        <h1 className = "signup-title">SIGN UP</h1>
+          <form onSubmit={handleSubmit}>
+            <div className = "signup-container">
+
+              <p className = "signup-categories">First Name</p>
+                <div className = "signup-user">
+                  <FaUser className="signup-icon-user" />
+                    <input
+                      id="signup-input-user"
+                      type="text"
+                      name="firstName"
+                      placeholder="First Name"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      required
+                    />
+                </div>
+
+              <p className = "signup-categories">Last Name</p>
+                <div className = "signup-user">
+                  <FaUser className="signup-icon-user" />
+                    <input
+                      id="signup-input-user"
+                      type="text"
+                      name="lastName"
+                      placeholder="Last Name"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      required
+                    />
+                </div>
+
+              <p className = "signup-categories">Mobile Number</p>
+                <div className = "signup-mobile">
+                  <FaMobileRetro className="signup-icon-mobile" />
+                  <p className = "signup-p-mobile">+63</p>
+                    <input
+                      id="signup-input-mobile"
+                      type="number"
+                      name="mobileNumber"
+                      placeholder="Mobile Number"
+                      value={formData.mobileNumber}
+                      onChange={handleChange}
+                      required
+                    />
+                </div>
+              
+              <p className = "signup-categories">Create Password</p>
+                <div className = "signup-pass">
+                  <FaLock className="signup-icon-mobile" />
+                    <input
+                      id="signup-input-user"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      required
+                    />
+                </div>
+
+              <p className = "signup-categories">Confirm Password</p>
+                <div className = "signup-pass">
+                  <FaLockOpen className="signup-icon-mobile" />
+                    <input
+                      id="signup-input-user"
+                      type="password"
+                      name="confirmPassword"
+                      placeholder="Confirm Password"
+                      onChange={handleChange}
+                      required
+                    />
+                </div>
+              
+              <p className = "signup-categories">Health Complications</p>
+                <div className = "signup-health">
+                  <FaNotesMedical className="signup-icon-health" />
+                  <select name="healthComplication" id="signup-input-health" onChange={handleChange} value={formData.healthComplication}>
+                    <option value="yes">I Have Health Complications</option>
+                    <option value ="no">I Don't Have Health Complications</option>
+                  </select>
+                </div>
+
+              </div>
+              
+
+            <div className='signup-low'>
+              {passErr && <div style={{color: 'red'}}>{passErr} </div>}
+              <button type="submit" className = "signup-submit">SIGN UP</button>
+              <p>Already have an account?<Link to ="/" className = "signup-login">Login</Link></p>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
     
   );
 }
