a133cc4698f36fd28e7d254908eb0cee5f2d3437
Tanchyy <angelo.tancioco@gmail.com>
2024-06-05 20:28:11 +0800

    added crud function into admin

--- djarshopper/djangoapi/admin.py
+++ djarshopper/djangoapi/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import LoginUser
+from .models import LoginUser, AddProduct
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('mobile_number', 'is_staff', 'is_active')
@@ -18,4 +18,17 @@ try:
 except admin.sites.NotRegistered:
     pass
 
+
 admin.site.register(LoginUser, UserAdmin)
+
+#this is for fetching data from the database to display products
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'ingredients', 'nutritional_facts')
+    search_fields = ('name',)
+    list_filter = ('price',)
+
+admin.site.register(AddProduct, ProductAdmin)
+
+
+    
--- djarshopper/djarshopper/settings.py
+++ djarshopper/djarshopper/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,10 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#redirecting custom URL from default admin to a dashboard
 # settings.py
-
-# LOGIN_REDIRECT_URL = '/dashboard/'
 
 
 # Internationalization
@@ -144,8 +141,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+#in order to store images from crud into react public folder
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# this handles the media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
