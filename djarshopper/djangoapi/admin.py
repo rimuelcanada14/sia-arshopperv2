46dec264831a48aeb6d63f4ff6565f918025f469
Tanchyy <angelo.tancioco@gmail.com>
2024-06-21 15:25:00 +0800

    added user information display in admin

--- djarshopper/djangoapi/admin.py
+++ djarshopper/djangoapi/admin.py
@@ -7,12 +7,12 @@ class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
 
-    list_display = ('mobile_number', 'is_staff', 'is_active')
-    search_fields = ('mobile_number',)
+    list_display = ('mobile_number', 'get_first_name', 'get_last_name', 'get_illness', 'get_illness2', 'get_illness3', 'is_staff', )
+    search_fields = ('mobile_number', 'signup__firstName', 'signup__lastName', 'signup__illness')
     ordering = ('mobile_number',)
     fieldsets = (
         (None, {'fields': ('mobile_number', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Permissions', {'fields': ('is_staff', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
@@ -21,8 +21,32 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
-admin.site.register(LoginUser, UserAdmin)
+    def get_first_name(self, obj):
+        return obj.signup.firstName
+    get_first_name.admin_order_field = 'signup__firstName'
+    get_first_name.short_description = 'First Name'
+    
+    def get_last_name(self, obj):
+        return obj.signup.lastName
+    get_last_name.admin_order_field = 'signup__lastName'
+    get_last_name.short_description = 'Last Name'
+    
+    def get_illness(self, obj):
+        return obj.signup.illness
+    get_illness.admin_order_field = 'signup__illness'
+    get_illness.short_description = 'Illness'
+    
+    def get_illness2(self, obj):
+        return obj.signup.illness2
+    get_illness2.admin_order_field = 'signup__illness'
+    get_illness2.short_description = '2nd Illness'
+    
+    def get_illness3(self, obj):
+        return obj.signup.illness3
+    get_illness3.admin_order_field = 'signup__illness'
+    get_illness3.short_description = '3rd Illness'
 
+admin.site.register(LoginUser, UserAdmin)
 #this is for fetching data from the database to display products
 
 class ProductAdmin(admin.ModelAdmin):
